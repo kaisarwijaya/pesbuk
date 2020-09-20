217f2f7ba98fb789f25d321d6c04e56b0871bbcb
Tubagus Kaisar wijaya <61956996+kaisarwijaya@users.noreply.github.com>
2020-09-20 23:12:08 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-        $db_host = ("cilsydb.ct6gyzehincn.ap-southeast-1.rds.amazonaws.com");
-        $db_user = ("root");
-        $db_pass = ("Admin123");
-        $db_name = ("pesbuk");
+        $db_host = "cilsydb.ct6gyzehincn.ap-southeast-1.rds.amazonaws.com";
+        $db_user = "root";
+        $db_pass = "Admin123";
+        $db_name = "pesbuk";
 
 try {    
     //create PDO connection 
